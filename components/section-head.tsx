c5460662ef232457dfec49e671aa2c3cc8e24a7b
adii1203 <aditya32ft@gmail.com>
2024-05-31 23:35:21 +0530

    feat: Add SectionHead component for displaying section titles and subtitles

---
+++ components/section-head.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SectionHead = ({
+  title,
+  subTitle,
+}: {
+  title: string;
+  subTitle: string;
+}) => {
+  return (
+    <div className="py-8 space-y-4">
+      <h2 className="text-2xl font-bold font-space border-border border-b-2 w-fit">
+        {title}
+      </h2>
+      <p className="font-space">{subTitle}</p>
+    </div>
+  );
+};
+
+export default SectionHead;

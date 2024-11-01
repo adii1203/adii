05f88bcb20be52bfbad5adef053c16e3aa3f545c
adii1203 <aditya32ft@gmail.com>
2024-11-02 01:28:49 +0530

    refactor: Update page layout and add ProjectCard component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import img from "@/public/project.jpg";
 import profileImg from "@/public/profile.png";
 import { SocialCard } from "@/components/social";
 import { Space_Grotesk } from "next/font/google";
 import SectionHead from "@/components/section-head";
-import { Button } from "@/components/ui/button";
-import { Badge } from "@/components/ui/badge";
 import TechStack from "@/components/tech-stact";
+import ProjectCard from "@/components/project-card";
+import { projectData } from "@/lib/project";
+import { BadgeProps } from "@/components/ui/badge";
 
 const SpaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
@@ -17,7 +17,7 @@ const SpaceGrotesk = Space_Grotesk({
 export default function Home() {
   return (
     <main
-      className={`max-w-[90%] md:max-w-[60%] mx-auto border-border border-l border-r border-dashed min-h-screen px-4 py-4 ${SpaceGrotesk.variable}`}>
+      className={`max-w-[90%] md:w-[80%] mx-auto border-border border-l border-r border-dashed min-h-screen px-4 py-4 ${SpaceGrotesk.variable}`}>
       <section
         className={`space-y-2 font-space border-border border-b border-double pt-6`}>
         <div className="w-12 rounded-full overflow-hidden">
@@ -41,72 +41,23 @@ export default function Home() {
           title="Projects"
           subTitle="A collection of projects that I have worked on."
         />
-        <div className="border bg-card p-2 rounded-md font-space xl:flex gap-4 space-y-4 w-fit xl:w-full mx-auto">
-          <div className="rounded overflow-hidden w-full xl:w-96 h-48 aspect-square">
-            <Image src={img} alt="" className="w-full h-full object-cover" />
-          </div>
-          <div className="flex-1 space-y-6">
-            <div className="flex items-center justify-between w-full">
-              <p className="capitalize text-lg font-bold">bookmark</p>
-              <div className="flex gap-2">
-                <Button variant={"outline"} size={"icon"} className="w-8 h-8">
-                  <a
-                    href="https://github.com/adii1203/bookmark"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round">
-                      <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4" />
-                      <path d="M9 18c-4.51 2-5-2-7-2" />
-                    </svg>
-                  </a>
-                </Button>
-                <Button variant={"outline"} size={"icon"} className="w-8 h-8">
-                  <a
-                    href="https://savedev.vercel.app"
-                    target="_blank"
-                    rel="noopener noreferrer">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeWidth="2"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      className="lucide lucide-arrow-up-right">
-                      <path d="M7 7h10v10" />
-                      <path d="M7 17 17 7" />
-                    </svg>
-                  </a>
-                </Button>
-              </div>
-            </div>
-            <div className="flex flex-col gap-4">
-              <p className="text-muted-foreground text-sm break-words md:w-full">
-                A simple bookmark manager that allows you to save your favorite
-                links.
-              </p>
-              <div className="flex gap-2 flex-wrap">
-                <Badge variant={"default"}>NextJS</Badge>
-                <Badge variant={"default"}>Postgres</Badge>
-                <Badge variant={"default"}>Typescript</Badge>
-                <Badge variant={"default"}>AuthJS</Badge>
-                <Badge variant={"default"}>Tanstack</Badge>
-                <Badge variant={"default"}>Tailwind</Badge>
-              </div>
-            </div>
-          </div>
+        <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 max-w-[800px] mx-auto">
+          {projectData.map((project, idx) => {
+            return (
+              <ProjectCard
+                key={idx}
+                title={project.title}
+                description={project.description}
+                techStack={project.techStack.map((item) => ({
+                  ...item,
+                  varient: item.varient as BadgeProps["variant"],
+                }))}
+                github={project.github}
+                live={project.live}
+                imageUrl={project.imageUrl}
+              />
+            );
+          })}
         </div>
       </section>
       <TechStack />
---
+++ components/project-card.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { Badge, badgeVariants } from "./ui/badge";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { VariantProps } from "class-variance-authority";
+
+type TechStackItem = {
+  text: string;
+  varient: VariantProps<typeof badgeVariants>["variant"];
+};
+
+type ProjectCardProps = {
+  title: string;
+  description: string;
+  techStack: TechStackItem[];
+  github?: string;
+  live?: string;
+  imageUrl: string;
+};
+
+const ProjectCard = ({
+  description,
+  techStack,
+  title,
+  github,
+  live,
+  imageUrl,
+}: ProjectCardProps) => {
+  return (
+    <div className="border rounded-lg overflow-hidden font-space">
+      <div className="aspect-video overflow-hidden object-contain">
+        <img src={imageUrl} alt="" />
+      </div>
+      <div className="px-2 pb-2 space-y-3">
+        <div>
+          <p className="font-bold text-lg">{title}</p>
+          <p className="text-xs text-foreground/50">{description}</p>
+        </div>
+        <div className="flex flex-wrap gap-1">
+          {techStack.map((item, idx) => {
+            return (
+              <Badge key={idx} variant={item.varient} className="">
+                {item.text}
+              </Badge>
+            );
+          })}
+        </div>
+        <div className="space-x-2">
+          {github && (
+            <Link
+              className={buttonVariants({
+                variant: "default",
+                className: "h-6",
+              })}
+              href={github}
+              target="_blank">
+              github
+            </Link>
+          )}
+          {
+            <Link
+              className={buttonVariants({
+                variant: "default",
+                className: "h-6",
+              })}
+              href={live!}
+              target="_blank">
+              live
+            </Link>
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
